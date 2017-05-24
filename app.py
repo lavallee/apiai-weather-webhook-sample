6dfb77a2aeb70688e7f3a1ef19a7a9ed7c37b3a8
lavallee <mlavallee@gmail.com>
2017-05-24 18:28:59 -0400

    wiring up 2

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def webhook():
 
 
 def processRequest(req):
-    print "in processRequest"
+    print("in processRequest")
     if req.get("result").get("action") != "yahooWeatherForecast":
         return {}
     baseurl = "https://query.yahooapis.com/v1/public/yql?"
